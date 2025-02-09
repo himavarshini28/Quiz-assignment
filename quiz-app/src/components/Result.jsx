0cfbc8e28a4999dacb7f34b99a622735ea0a9903
himavarshini28 <himavarshinimahapatruni@gmail.com>
2025-02-09 13:58:36 +0530

    added questions and tracking score

--- quiz-app/src/components/Result.jsx
+++ quiz-app/src/components/Result.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import {useLocation} from 'react-router-dom';
 function Result() {
+
+  const location=useLocation();
+  const { score, attempted } = location.state || {score:0, attempted:0};
   return (
     <div>
-      
+       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">Quiz Completed! 🎉</h1>
+      <p className="text-xl font-semibold text-gray-700">Score: {score}</p>
+      <p className="text-lg text-gray-600">Questions Attempted: {attempted}</p>
+    </div>
     </div>
   )
 }
